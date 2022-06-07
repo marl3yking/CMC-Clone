5c8c12f98264974432706aa1a2aecb9f1fb6dcc8
Your Name <devinmcelroy10@gmail.com>
2022-06-07 16:36:49 -0700

    ADD: Header frontend

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains: ['s2.coinmarketcap.com', 's3.coinmarketcap.com', 'encrypted-tbn0.gstatic.com']
+  },
+}
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div className='min-h-screen'>
+      <Header />
+      <div className='mt-10'/>
+      {/* <Trending /> */}
+      <div className='mt-20'/>
+      {/* <CMCtable /> */}
+    </div>
+  )
+}
+
+export default Home
